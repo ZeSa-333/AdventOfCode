693f82a9413f0255226df2a1f34ffe408374b231
Sarah Zeitler <snzeitler333@gmail.com>
2023-12-07 10:56:33 +0100

    Day 6.2 but make it oop

---
+++ Day6/Day6.2_OOP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml;
+
+public class Race 
+{
+    long time;
+    long distance;
+    public Race(long _time, long _distance)
+    {
+        time = _time;
+        distance = _distance;
+    }
+
+    public int countTimesBeat ()
+    {
+        int counter = 0;
+        for (long i = 0; i < time; i++)
+        {
+            long timeLeft = time - i;
+
+            if ((timeLeft * i) > distance)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
+public class MainClass{
+public static void Main(string[] args)
+{
+    if(args.Length > 0){
+    string[] input = File.ReadAllLines(args[0]);
+    long time = long.Parse(input[0].Split(":")[1].Trim().Replace(" ", String.Empty));
+    long distance = long.Parse(input[1].Split(":")[1].Trim().Replace(" ", String.Empty));
+
+    Race race = new Race(time, distance);
+    Console.WriteLine(race.countTimesBeat());
+    }
+    else if (args.Length == 0)
+    {
+        Console.WriteLine("Please enter a file name");
+    }
+}
+}
